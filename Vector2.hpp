2143e2f5394a6ff858468addd72961b2985ee0af
ManildoDiego <diegothiry27@gmail.com>
2024-04-13 13:14:55 -0300

    added constant expressions

--- Vector2.hpp
+++ Vector2.hpp
@@ -28,27 +28,27 @@ public:
     static constexpr Vector2<int> UP = Vector2(0, -1);
     static constexpr Vector2<int> DOWN = Vector2(0, 1);
 
-    Vector2 abs() const noexcept {
+    constexpr Vector2 abs() const noexcept {
         return Vector2(std::abs(m_x), std::abs(m_y));
     }
 
-    T angle() const noexcept {
+    constexpr T angle() const noexcept {
         return std::atan2(m_y, m_x);
     }
 
-    T angle_to(const Vector2& other) const noexcept {
+    constexpr T angle_to(const Vector2& other) const noexcept {
         return std::atan2(other.m_y - m_y, other.m_x - m_x);
     }
 
-    T angle_to_point(T x, T y) const noexcept {
+    constexpr T angle_to_point(T x, T y) const noexcept {
         return angle_to(Vector2(x, y));
     }
 
-    T aspect() const noexcept {
+    constexpr T aspect() const noexcept {
         return m_x / m_y;
     }
 
-    Vector2 bounce(const Vector2& normal) const noexcept {
+    constexpr Vector2 bounce(const Vector2& normal) const noexcept {
         const auto dotProduct = m_x * normal.m_x + m_y * normal.m_y;
         
         return Vector2(
@@ -57,22 +57,22 @@ public:
         );
     }
 
-    Vector2 ceil() const noexcept {
+    constexpr Vector2 ceil() const noexcept {
         return Vector2(std::ceil(m_x), std::ceil(m_y));
     }
 
-    Vector2 clamp(const Vector2& min, const Vector2& max) const noexcept {
+    constexpr Vector2 clamp(const Vector2& min, const Vector2& max) const noexcept {
         return Vector2(
             std::clamp(m_x, min.m_x, max.m_x),
             std::clamp(m_y, min.m_y, max.m_y)
         );
     }
 
-    T cross(const Vector2& with) const noexcept {
+    constexpr T cross(const Vector2& with) const noexcept {
         return m_x * with.m_y - m_y * with.m_x;
     }
 
-    Vector2 cubic_interpolate(const Vector2& b, const Vector2& pre_a, const Vector2& post_b, T weight) const noexcept {
+    constexpr Vector2 cubic_interpolate(const Vector2& b, const Vector2& pre_a, const Vector2& post_b, T weight) const noexcept {
         const auto t2 = weight * weight;
         const auto t3 = t2 * weight;
 
@@ -82,16 +82,16 @@ public:
         );
     }
 
-    Vector2 direction_to(const Vector2& to) const noexcept {
+    constexpr Vector2 direction_to(const Vector2& to) const noexcept {
         const auto displacement = to - *this;
         return displacement.normalized();
     }
 
-    Vector2 normalized() const noexcept {
+    constexpr Vector2 normalized() const noexcept {
         const auto length = std::sqrt(m_x * m_x + m_y * m_y);
         return (length != 0) ? Vector2(m_x / length, m_y / length) : *this;
     }
 private:
-    T m_x;
-    T m_y;
+    constexpr T m_x;
+    constexpr T m_y;
 };
