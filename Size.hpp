a13e1a3686164827dc1c1140350c322e91eb87a5
ManildoDiego <diegothiry27@gmail.com>
2024-04-14 10:13:22 -0300

    converted width and height to public

--- Size.hpp
+++ Size.hpp
@@ -2,20 +2,19 @@
 
 class Size {
 public:
+    int width;
+    int height;
+
     constexpr Size() noexcept = default;
-    constexpr Size(int width, int height) noexcept : m_width(width), m_height(height) {}
+    constexpr Size(int w, int h) noexcept : width(w), height(h) {}
     constexpr Size(const Size&) noexcept = default;
     constexpr Size(Size&&) noexcept = default;
 
     constexpr Size& operator=(const std::pair<int, int> pair) noexcept {
-        m_width = pair.first;
-        m_height = pair.second;
+        width = pair.first;
+        height = pair.second;
     }
 
     constexpr Size& operator=(const Size&) noexcept = default;
     constexpr Size& operator=(Size&&) noexcept = default;
-
-private:
-    int m_width;
-    int m_height;
 };
